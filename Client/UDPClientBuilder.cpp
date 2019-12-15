0ddd4675c2b898a23b51185b74ff70fc61267e27
araneoAnicet <araneoanicet@gmail.com>
2019-12-15 20:31:58 +0100

    add UDPClient builder methods definitions

---
+++ Client/UDPClientBuilder.cpp
@@ -0,0 +1,21 @@
+#include "UDPClientBuilder.h"
+
+void UDPClientBuilder::setHost() {
+    std::cout << "Enter your targer ip:" << std::endl;
+    std::cout << ">>> ";
+    std::string userInput;
+    std::cin >> userInput;
+    this->hostAddr = boost::asio::ip::address_v4::from_string(userInput);
+}
+
+void UDPClientBuilder::setHost(boost::asio::ip::address_v4 addr) {
+    this->hostAddr = addr;
+}
+
+void UDPClientBuilder::setPort(int port) {
+    this->port = port;
+}
+
+UDPClient* UDPClientBuilder::build() {
+    return new UDPClient(this);
+}
